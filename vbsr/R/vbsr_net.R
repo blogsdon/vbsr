df10308fe4611978cd4dadeeb275b8b1b8c9d170
Benjamin Logsdon <blogsdon@cs.washington.edu>
2013-08-01 21:10:06 +0000

    added nice estimated beta and z statistics

--- vbsr/R/vbsr_net.R
+++ vbsr/R/vbsr_net.R
@@ -13,6 +13,7 @@ vbsr_net = function(y,
 		estimation_type = "BMA",
 		bma_approximation = TRUE,
 		screen = 1.0,
+		bonf_l0=TRUE,
 		already_screened = 1.0,
 		kl = 0.99,
 		l0_path=NULL,
@@ -24,7 +25,10 @@ vbsr_net = function(y,
 		X <- cbind(rep(1,n),X);
 		m <- m+1;
 	}
-
+	if(bonf_l0_{
+		path_length=1;
+		l0_path=-(qchisq(0.05/m,1,lower.tail=FALSE)-log(n)+2*log(0.05/0.95));
+	}
 	#n <- nrow(X);
 
 	#m <- ncol(X);
@@ -509,6 +513,10 @@ vbsr_net = function(y,
 			result_list$kl_index <- b_list;
 
 		}
-	}		
+	}	
+	if(bonf_l0){
+		result_list$beta <- result_list$e_beta[-wexc];
+		result_list$z <- result_list$beta_chi[-wexc];
+	}
 	return(result_list);
 }
