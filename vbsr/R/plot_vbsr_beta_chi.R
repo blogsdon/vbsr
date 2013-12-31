68309424d0d0a443e1db76ccc8e2bf5e037af5d3
Benjamin Logsdon <aeberhart@benjamin-logsdons-macbook-pro.local>
2013-12-31 10:30:23 -0800

    Get rid of deprecated functions.

--- vbsr/R/plot_vbsr_beta_chi.R
+++
@@ -1,9 +0,0 @@
-plot_vbsr_beta_chi = function(res){
-
-
-	bp <- res$beta_chi[-res$which_excluded,];
-	m <- nrow(bp);
-
-	matplot((matrix(rep(res$l0_path,m),length(res$l0_path),m)),t(bp),'l',xlab=expression(l[0]),ylab=expression(p[j]),col=rainbow(50),lwd=2,main=expression(beta),cex.axis=1,cex.lab=1);
-
-}
