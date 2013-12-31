0f5374f5077940481e58f9ba419bd17dc7619f91
Benjamin Logsdon <aeberhart@benjamin-logsdons-macbook-pro.local>
2013-12-31 11:13:57 -0800

    merged the truncated normal sampler into compute_KL.  Finished first pass of documentation.

--- vbsr/R/compute_KL.R
+++ vbsr/R/compute_KL.R
@@ -1,5 +1,29 @@
 compute_KL = function(Zmat,alpha,pval){
-
+  #define function to sample from truncated normal distribution.
+  rtnorm <- function(n,mu,sigma,a,b){
+    accept <- rep(0,n);
+    x <- rep(0,n);
+    u <- rep(0,n);
+    if(a>mu){
+      l<-a;
+    } else if(b<mu){
+      l<-b;
+    } else{
+      l<-mu;
+    }
+    wh <- 1:n;
+    while(sum(accept)<n){
+      x[wh] <- runif(length(wh),a,b);
+      u[wh] <- runif(length(wh),0,1);
+      ntrue <- which(u[wh]<dnorm(x[wh],mu,sigma)/dnorm(l,mu,sigma));
+      accept[wh][ntrue]<-TRUE;
+      wh<-which(accept==FALSE);
+    }
+    return(x);
+  }
+  
+  
+  
 	n <- nrow(Zmat);
 	m <- ncol(Zmat);
 	kl_vec <- rep(0,m);
